6a31ded45f6c578bc9154ff78792a5dc4f21f85c
Jett Seale <sealee@oregonstate.edu>
2020-03-03 19:02:28 -0800

    got rid of weird bug where js files wouldn't connect

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 app.get('*', (req, res) => {
   if (req.url === '/') res.sendFile(path.join(`${__dirname}/public/html/index.html`));
   else if (req.url === '/style.css') res.sendFile(path.join(`${__dirname}/public/css/style.css`));
+  else if (req.url === '/index.js') res.sendFile(path.join(`${__dirname}/public/js/index.js`))
   else res.sendFile(path.join(`${__dirname}/public/html/404.html`));
 });
 
