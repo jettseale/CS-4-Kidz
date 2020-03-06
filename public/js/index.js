94659dbb5f2f1c87674c816a69e0b01b6546326a
Matthew Mazon <mazonm@oregonstate.edu>
2020-03-06 11:59:55 -0800

    Added Username

--- public/js/index.js
+++ public/js/index.js
@@ -10,27 +10,20 @@ var count = 0;
   $.getJSON("./email-data.json", function(obj){
     var countForObject = obj.length;
     for (var i = 0; i < countForObject; i++){
-
     if ( obj[i].email == form.elements[0].value && obj[i].password == form.elements[1].value )   {
-
       var emailstring ="Email: " + obj[i].email;
       var passstring = "Pass: " + obj[i].password;
-      alert(emailstring + " " + passstring + "\n Valid Credentials");
+      var user = "Username: " + obj[i].username;
+      alert(emailstring + "\n" + passstring +  "\n" + user + "\n" + "Valid Credentials");
       count++;
       break;
-
     }
-
     }
 
     if (count == 0 ) {
     alert("Not A Valid Email or Password");
 }
 
-    //for (int i = 0; i)
-    //if ( ) {
-
-    //}
   });
 
   event.preventDefault();
